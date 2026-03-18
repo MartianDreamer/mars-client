70f65e42efc17a39e509ed3b731b0d504463451d
MartianDreamer <nguyenxuansang9494@gmail.com>
2026-03-18 20:09:21 +0700

    RequestForm

--- webview/src/App.tsx
+++ webview/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { RequestForm } from './components/request/RequestForm'
 
 function App() {
 
   return (
     <>
     <h1>Hello, World</h1>
+    <RequestForm />
     </>
   )
 }
---
+++ webview/src/components/request/RequestForm.tsx
@@ -0,0 +1,47 @@
+export const RequestForm = () => {
+  return (
+    <>
+      <div className="input-group mb-3">
+        <button
+          className="btn btn-outline-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Method
+        </button>
+        <ul className="dropdown-menu">
+          <li>
+            <a className="dropdown-item" href="#">
+              Action
+            </a>
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Another action
+            </a>
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Something else here
+            </a>
+          </li>
+          <li>
+            <hr className="dropdown-divider" />
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Separated link
+            </a>
+          </li>
+        </ul>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Text input with dropdown button"
+          placeholder="Enter URL"
+        />
+      </div>
+    </>
+  );
+};
